3b57053b80705cc55b4087853b7cf428c7e71156
RStyle <3441529+RStyle@users.noreply.github.com>
2024-06-26 21:38:17 +0200

    created admin pages

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,11 @@
-this page can only be seen by the admins: RStyle, Dmark, Gamer0x and HYPE
+<script>
+	import { Button } from 'flowbite-svelte';
+</script>
+
+this page can only be seen by the admins: RStyle, Dmark, Gamer0x, Chauvi and HYPE
+
+<ul>
+	<li>
+		<Button href="/admin/event-managment">event-managment</Button>
+	</li>
+</ul>
---
+++ src/routes/admin/event-managment/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.params.fullEventName === undefined}
+	hello
+{:else}
+	hi
+{/if}
